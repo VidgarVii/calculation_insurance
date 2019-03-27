70bfcb5a76e8e8698895fdf1f65aa6d447da5422
Pavel <vidasp@mail.ru>
2019-03-27 17:20:24 +0300

    Add help

--- main.rb
+++ main.rb
@@ -1,3 +1,18 @@
 require_relative 'lib/calculator'
 require_relative 'lib/application_view'
 require 'ostruct'
+
+# Get started
+#
+# client = OpenStruct.new(goods_cost: 30_000,
+#                         downpayment: 3000,
+#                         term: 12,
+#                         age: 44,
+#                         employment: :own_business,
+#                         insurances: %i[life job])
+#
+# app = ApplicationView.new
+#
+# app.render_to_console_for(client)
+# app.render_to_html_for(client)
+#
